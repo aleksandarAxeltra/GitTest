15025dafde81f7954dd73097623a1fb857c366de
Aleksandar Karafilovski <a.karafilovski@axeltra.com>
2014-11-14 11:34:06 +0100

    added log in master

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.gittest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,5 +13,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("awd","awd");
     }
 }
