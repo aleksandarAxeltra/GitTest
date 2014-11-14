ff30ea05b4ae14f065fb0bd301dfa964f986ab9d
Aleksandar Karafilovski <a.karafilovski@axeltra.com>
2014-11-14 10:57:28 +0100

    onResume added

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -13,4 +13,9 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 }
