d752edf639a39f784aee402f69b789a6d2a4a59e
Aleksandar Karafilovski <a.karafilovski@axeltra.com>
2014-11-14 11:17:24 +0100

    menu added to master

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.gittest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,5 +13,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Log.i("awd","awd");
+        return super.onCreateOptionsMenu(menu);
     }
 }
